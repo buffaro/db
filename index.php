14928e704a8a870b57f1978b770a8cf62b9d9f12
buffaro <buffaro_master@hotmail.com>
2022-08-08 16:01:10 +0700

    2
    add form

--- index.php
+++ index.php
@@ -6,18 +6,46 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>บริษัท ไอโซเทค อินสตรูเมนท์ (ไทยแลนด์) จำกัด</title>
-  <link rel="icon" type="image/x-icon" href="./assets/images/isologo.png" />
-  <link rel="stylesheet" href="css/bootstrap.min.css" />
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/jquery-3.6.0.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.13.1/underscore-umd-min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.13.1/underscore-esm-min.js"></script>
+  <link rel="icon" type="image/x-icon" href="../assets/images/isologo.png" />
+  <link rel="stylesheet" href="../css/bootstrap.min.css" />
+  <script src="../js/bootstrap.min.js"></script>
+  <script src="../js/jquery-3.6.0.min.js"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 </head>
 
 <body>
-
+  <div class="container">
+    <h2>Devices Data</h2>
+    <div class="mb-3">
+      <label for="hos_code" class="form-label">Hospital Code</label>
+      <input type="text" class="form-control" id="hos_code" placeholder="CHC-00000">
+    </div>
+    <div class="mb-3">
+      <label for="ref_code" class="form-label">Reference Code</label>
+      <input type="text" class="form-control" id="ref_code" placeholder="CPY-XX-00">
+    </div>
+    <div class="mb-3">
+      <label for="ref_code" class="form-label">Device Name</label>
+      <input type="text" class="form-control" id="ref_code" placeholder="">
+    </div>
+    <div class="mb-3">
+      <label for="brand" class="form-label">Brand</label>
+      <input type="text" class="form-control" id="brand" placeholder="">
+    </div>
+    <div class="mb-3">
+      <label for="model" class="form-label">Model</label>
+      <input type="text" class="form-control" id="model" placeholder="">
+    </div>
+    <div class="mb-3">
+      <label for="sn" class="form-label">Serial Number</label>
+      <input type="text" class="form-control" id="sn" placeholder="">
+    </div>
+    <div class="mb-3">
+      <label for="loc" class="form-label">Location</label>
+      <input type="text" class="form-control" id="loc" placeholder="">
+    </div>
+  </div>
 </body>
 
 </html>
